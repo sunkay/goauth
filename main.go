17607ea075ab50ce14cf5a03fde79c648068f0d5
sunkay <sunkay11@gmail.com>
2017-09-17 15:38:18 -0400

    google oauth server side implementation

--- main.go
+++ main.go
@@ -1,17 +1,93 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/julienschmidt/httprouter"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
+type credentials struct {
+	Cid     string `json:"cid"`
+	Csecret string `json:"csecret"`
+}
+
+var creds credentials
+var oconf *oauth2.Config
+
+func init() {
+	// Read credentials from creds.json
+	file, err := ioutil.ReadFile("./creds.json")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		panic(err)
+	}
+
+	json.Unmarshal(file, &creds)
+
+	//configure oAuth2 struct
+	oconf = &oauth2.Config{
+		ClientID:     creds.Cid,
+		ClientSecret: creds.Csecret,
+		RedirectURL:  "http://localhost:3000/auth/google/callback",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+		},
+		Endpoint: google.Endpoint,
+	}
+
+}
+
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Go Auth Router... ")
+	w.Header().Set("Content-Type", "application/json")
+
+	type profile struct {
+		First string
+		Last  string
+	}
+
+	p := profile{"sun", "kay"}
+	js, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println("json:", js)
+
+	w.Write(js)
+}
+
+func authGoogle(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.Redirect(w, r, oconf.AuthCodeURL("safe"), http.StatusMovedPermanently)
+}
+
+func authGoogleCallback(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// retrieve query param code
+	queryValues := r.URL.Query()
+	code := queryValues.Get("code")
+	fmt.Println("Google code: ", code)
+
+	// convert code into a token
+	tok, _ := oconf.Exchange(oauth2.NoContext, code)
+
+	// Client returns an authorized HTTP Client using the provided token
+	client := oconf.Client(oauth2.NoContext, tok)
+
+	// get the information using the http client
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	data, _ := ioutil.ReadAll(resp.Body)
+	w.Write(data)
 }
 
 func main() {
@@ -19,8 +95,9 @@ func main() {
 
 	router := httprouter.New()
 	router.GET("/", index)
-	router.GET("/xyz", index)
+	router.GET("/auth/google", authGoogle)
+	router.GET("/auth/google/callback", authGoogleCallback)
 
-	log.Fatal(http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, router)))
+	log.Fatal(http.ListenAndServe(":3001", handlers.LoggingHandler(os.Stdout, router)))
 
 }
